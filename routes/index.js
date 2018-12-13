c951efa6f549eff9a42e3e4b44044d0ee6ff62f3
redowls <lukitowisley@gmail.com>
2018-12-13 12:40:39 +0700

    intial

---
+++ routes/index.js
@@ -0,0 +1,39 @@
+var express = require('express');
+var router = express.Router();
+
+var passport = require('passport');
+var FacebookStrategy = require('passport-facebook').Strategy;
+
+router.use(passport.initialize());
+
+passport.use(new FacebookStrategy({
+  clientID : '268144707236012',
+  clientSecret : 'c3be08fa3c001783101a1e8300b99cc9',
+  callbackURL: 'https://0374a66c.ap.ngrok.io/authFacebook/done',
+  profileFields: ['id', 'name', 'email', 'photos']
+}, function(accessToken, refreshToken, profile, done){
+  return done(null, profile);
+}))
+
+passport.serializeUser(function(profile,done){
+  return done(null, profile);
+})
+
+passport.deserializeUser(function(profile,done){
+  return done(null, profile);
+})
+
+router.get('/authFacebook', passport.authenticate('facebook'));
+router.get('/authFacebook/done', 
+passport.authenticate('facebook', {
+  failureRedirect: '/'
+}),function(req,res){
+  return res.json(req.user);
+});
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+module.exports = router;
